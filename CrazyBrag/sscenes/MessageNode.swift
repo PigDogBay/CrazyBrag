b2d2847dc7acbd97e7eb80026605a3246c1aa156
Mark Bailey <markpdbailey@gmail.com>
2023-12-06 19:39:25 +0000

    Bug fix, empty string was blowing up Attributed Text

--- CrazyBrag/sscenes/MessageNode.swift
+++ CrazyBrag/sscenes/MessageNode.swift
@@ -9,6 +9,7 @@ import Foundation
 import SpriteKit
 
 class MessageNode : SKLabelNode {
+    ///Uses attributedText so that multi-line text can be center justified
     let attributes: [NSAttributedString.Key : Any]
 
     override init(){
@@ -33,10 +34,18 @@ class MessageNode : SKLabelNode {
         fatalError("init(coder:) has not been implemented")
     }
     
+
+    ///Fades out the previous message and then fades in the new
     func show(message : String) {
         let fadeOut = SKAction.fadeOut(withDuration: 0.5)
-        let fadeIn = SKAction.fadeIn(withDuration: 0.5)
+        //NOTA BENE: an empty string will blow up attributedText with NSMutableRLEArray out of bounds exception
+        //Trap it here and just fade out the existing text
+        if (message.isEmpty){
+            run(fadeOut)
+            return
+        }
 
+        let fadeIn = SKAction.fadeIn(withDuration: 0.5)
         let setText = SKAction.run { [weak self] in self?.attributedText = NSAttributedString(string: message, attributes: self?.attributes)}
         let sequence = SKAction.sequence([fadeOut,setText,fadeIn])
         run(sequence)
