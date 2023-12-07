4087f14f730a3239f552dda2df4d3d42d8f000d1
Mark Bailey <markpdbailey@gmail.com>
2023-12-07 13:02:06 +0000

    Renamed mad dog to pig dog

--- CrazyBrag/model/School.swift
+++ CrazyBrag/model/School.swift
@@ -26,7 +26,7 @@ class School {
     }()
     lazy var playerHolly : Player = {
         let ai = OneDownAI()
-        let player = Player(name: "Mad Dog", ai: ai)
+        let player = Player(name: "Pig Dog", ai: ai)
         ai.player = player
         return player
     }()
